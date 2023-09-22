467e01032344392902ebc199d0121afa90161ebc
Bryan <bryanwong92@gmail.com>
2023-09-21 23:17:48 -0400

    CSS Changes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,19 @@
 class MessagesController < ApplicationController
  before_action :authenticate_user!
 
- def create
-  @message = current_user.messages.build(message_params)
+def create
+ @message = current_user.messages.build(message_params)
 
-  if @message.save
-   TwilioService.send_sms(to: @message, text: @message.text)
+ if @message.save
+   TwilioService.send_sms(to: @message.recipient_phone, text: @message.text)
    redirect_to messages_path, notice: 'Message sent!'
-  else
-   render :new, alert: 'Message not sent!'
-  end
+ else
+   flash.now[:alert] = @message.errors.full_messages.join(', ')
+   render :new
  end
+end
+
+
 
  def index
   @messages = current_user.messages.order(created_at: :desc)
@@ -20,13 +23,18 @@ class MessagesController < ApplicationController
   end
  end
 
-
  def new
   @message = Message.new
  end
+
+ def count
+  count = Message.count
+  render json: { message_count: count }
+ end
+
  private
 
  def message_params
-  params.require(:message).permit(:to, :text)
+  params.require(:message).permit(:recipient_phone, :text)
  end
 end
