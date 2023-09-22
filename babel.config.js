cff60d33bfb8248d33800618585ec9414036d8eb
Bryan <bryanwong92@gmail.com>
2023-09-22 10:24:40 -0400

    Removed babel private method

--- babel.config.js
+++ babel.config.js
@@ -53,18 +53,18 @@ module.exports = function(api) {
           useBuiltIns: true
         }
       ],
-      [
-        '@babel/plugin-proposal-private-methods',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-private-property-in-object',
-        {
-          loose: true
-        }
-      ],
+      // [
+      //   '@babel/plugin-proposal-private-methods',
+      //   {
+      //     loose: true
+      //   }
+      // ],
+      // [
+      //   '@babel/plugin-proposal-private-property-in-object',
+      //   {
+      //     loose: true
+      //   }
+      // ],
       [
         '@babel/plugin-transform-runtime',
         {
