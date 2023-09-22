f33ce51562a2eed6d884a21d90d9caa4be3a6142
Bryan <bryanwong92@gmail.com>
2023-09-21 23:53:11 -0400

    Add Username
    Add Username to sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :messages
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, if: :will_save_change_to_email?
 end
