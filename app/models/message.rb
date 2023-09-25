701e65255ca03a716f957f0fc6e4a808e1a481d1
Bryan <bryanwong92@gmail.com>
2023-09-25 19:32:07 -0400

    Cleanup
    *Added after submission*
    *Removed migrations that were not needed
    *Removed gems not needed
    *Removed additional lines and combined
    *Added format validation in message model
    *Added more test cases and removed additional lines
    *Update Gemfile.lock
    *Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :recipient_phone, presence: true, format: { with: /\d{10}/, message: "Valid number needed" }
-  validates_length_of :text, maximum: 250
   validates :text, presence: true, length: { maximum: 250 }
 end
