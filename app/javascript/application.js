0dc3109cd23fa2b57b8521052c39d2014bb3ffed
Bryan <bryanwong92@gmail.com>
2023-09-25 23:04:14 -0400

    Additional Cleanup
    *Condensed javascript files into one file
    *CSS for Signout Button
    *Basic cleanup
    *Rename module
    *Sanitize phone number

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,32 +1,77 @@
-var app = angular.module('myApp', []);
+var app = angular.module('SMSMessengerApp', []);
 
-app.controller('MessagesController', ['$scope', '$http', function($scope, $http) {
+app.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] = angular.element('meta[name=csrf-token]').attr('content');
+}]);
+
+app.controller('MessagesController', ['$scope', '$http', function ($scope, $http) {
+    $scope.newMessage = {
+        recipient_phone: '',
+        text: ''
+    };
+
+    $scope.errorMessage = '';
     $scope.messages = [];
-    $scope.newMessage = {};
-    $scope.showNewMessageForm = false;
 
-    $scope.loadMessages = function() {
-        $http.get('/messages.json').then(function(response) {
-            $scope.messages = response.data;
-        });
+    $scope.sendMessage = function () {
+        if (!isValidPhoneNumber($scope.newMessage.recipient_phone)) {
+            $scope.errorMessage = 'Invalid phone number';
+            return;
+        }
+
+        $scope.newMessage.recipient_phone = $scope.newMessage.recipient_phone.replace(/-/g, '');
+
+        $http.post('/messages.json', $scope.newMessage).then(
+            function (response) {
+                $scope.messages.push(response.data);
+                $scope.newMessage = {
+                    recipient_phone: '',
+                    text: ''
+                };
+                loadMessagesCount();
+                loadMessages();
+                $scope.errorMessage = '';
+            },
+            function (error) {
+                console.error('Error:', error);
+                $scope.errorMessage = 'Can not save!';
+            }
+        );
     };
 
-    $scope.sendMessage = function() {
-        $http.post('/messages.json', $scope.newMessage).then(function(response) {
-            $scope.messages.push(response.data);
-            $scope.newMessage = {};
-        }, function(error) {
-            console.error('Error sending message:', error);
-        });
+    $scope.clear = function () {
+        $scope.newMessage.recipient_phone = '';
+        $scope.newMessage.text = '';
+        $scope.errorMessage = '';
     };
 
-    $scope.grabMessagesCount = function() {
+    function isValidPhoneNumber(phoneNumber) {
+        return /^\d{10}$/.test(phoneNumber) || /^\d{3}-\d{3}-\d{4}$/.test(phoneNumber);
+    }
+
+    function formatPhoneNumber(phoneNumber) {
+        return phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+    }
+
+    function loadMessagesCount() {
         $http.get('/messages/count.json').then(function (response) {
             $scope.messageCount = response.data.message_count;
         }, function (error) {
             console.error('Error', error);
         });
-    };
+    }
 
-    $scope.loadMessages();
+    function loadMessages() {
+        $http.get('/messages.json').then(function (response) {
+            $scope.messages = response.data.map(message => {
+                message.recipient_phone = formatPhoneNumber(message.recipient_phone);
+                return message;
+            });
+        }, function (error) {
+            console.error('Messages not loaded:', error);
+        });
+    }
+
+    loadMessagesCount();
+    loadMessages();
 }]);
